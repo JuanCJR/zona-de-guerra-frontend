9b795d5ea46a207a7057b1891ece82330daf63c9
Juan Carlos Jimenez <juancarlos.jimenez@coredevx.com>
2021-08-26 17:24:35 -0400

    update

--- src/components/PageTemplate.js
+++ src/components/PageTemplate.js
@@ -46,98 +46,101 @@ export const PageTemplate = (props) => {
       {/** Header */}
       <header>
         <Container fluid>
-        <Navbar expand="lg">
-          <Navbar.Brand className="mx-auto">
-            Zona de Guerra Paintball
-          </Navbar.Brand>
-        </Navbar>
-              <Navbar expand="lg">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse>
-                  <Nav className="mx-auto">
-                    <Nav.Link
-                      className="me-5 ms-5"
-                      onClick={() => handleClick("/")}
-                    >
-                      Inicio
-                    </Nav.Link>
-                    <Nav.Link
-                      className="me-5 ms-5"
-                      onClick={() => handleClick("/tarifas")}
-                    >
-                      Tarifas
-                    </Nav.Link>
-                    <Nav.Link
-                      className="me-5 ms-5"
-                      onClick={() => handleClick("/galeria")}
-                    >
-                      Galeria
-                    </Nav.Link>
-                    <Nav.Link
-                      className="me-5 ms-5"
-                      onClick={() => handleClick("/reservar")}
-                    >
-                      Reservar
-                    </Nav.Link>
-                    <Nav.Link
-                      className="me-5 ms-5"
-                      onClick={() => handleClick("/contacto")}
-                    >
-                      Contacto
-                    </Nav.Link>
-                  </Nav>
-                  <Nav className="ms-auto">
-                    <Nav.Link>
-                      <img
-                        className="me-2"
-                        width="30"
-                        src="img/facebook.png"
-                        alt="facebook icon"
-                      />
-                    </Nav.Link>
-                    <Nav.Link href="https://www.instagram.com/guerrapaintballjm/?hl=es" target="_blank">
-                      <img
-                        className="me-2"
-                        width="30"
-                        src="img/instagram.png"
-                        alt="instagram icon"
-                      />
-                    </Nav.Link>
-                    {state.isLoggedIn ? (
-                    <>
-                        <Nav.Link
-                        className="me-5 ms-5"
-                        onClick={() => handleClick("/usuarios")}
-                      >
-                        Usuarios
-                      </Nav.Link>
-                       <Nav.Link
-                       className="me-5 ms-5"
-                       onClick={() => handleClick("/admin")}
-                     >
-                       Reservas
-                     </Nav.Link>
-                    </>
-                    ) : (
-                      <React.Fragment></React.Fragment>
-                    )}
-                  </Nav>
-                </Navbar.Collapse>
-
+          <Navbar expand="lg">
+            <Navbar.Brand className="mx-auto">
+              Zona de Guerra Paintball
+            </Navbar.Brand>
+          </Navbar>
+          <Navbar expand="lg">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse>
+              <Nav className="mx-auto">
+                <Nav.Link
+                  className="me-5 ms-5"
+                  onClick={() => handleClick("/")}
+                >
+                  Inicio
+                </Nav.Link>
+                <Nav.Link
+                  className="me-5 ms-5"
+                  onClick={() => handleClick("/tarifas")}
+                >
+                  Tarifas
+                </Nav.Link>
+                <Nav.Link
+                  className="me-5 ms-5"
+                  onClick={() => handleClick("/galeria")}
+                >
+                  Galeria
+                </Nav.Link>
+                <Nav.Link
+                  className="me-5 ms-5"
+                  onClick={() => handleClick("/reservar")}
+                >
+                  Reservar
+                </Nav.Link>
+                <Nav.Link
+                  className="me-5 ms-5"
+                  onClick={() => handleClick("/contacto")}
+                >
+                  Contacto
+                </Nav.Link>
+              </Nav>
+              <Nav className="ms-auto">
+                <Nav.Link>
+                  <img
+                    className="me-2"
+                    width="30"
+                    src="img/facebook.png"
+                    alt="facebook icon"
+                  />
+                </Nav.Link>
+                <Nav.Link
+                  href="https://www.instagram.com/guerrapaintballjm/?hl=es"
+                  target="_blank"
+                >
+                  <img
+                    className="me-2"
+                    width="30"
+                    src="img/instagram.png"
+                    alt="instagram icon"
+                  />
+                </Nav.Link>
                 {state.isLoggedIn ? (
-                  <Navbar.Collapse>
-                    <Navbar.Text style={{ fontSize: "0.8rem" }}>
-                      Usuario: {state.nom_usu}
-                    </Navbar.Text>
-                    <Button className="ms-3" onClick={onLogOut}>
-                      Salir
-                    </Button>
-                  </Navbar.Collapse>
+                  <>
+                    <Nav.Link
+                      className="me-5 ms-5"
+                      onClick={() => handleClick("/usuarios")}
+                    >
+                      Usuarios
+                    </Nav.Link>
+                    <Nav.Link
+                      className="me-5 ms-5"
+                      onClick={() => handleClick("/admin")}
+                    >
+                      Reservas
+                    </Nav.Link>
+                  </>
                 ) : (
                   <React.Fragment></React.Fragment>
                 )}
-              </Navbar>
-              </Container>
+              </Nav>
+            </Navbar.Collapse>
+
+            {state.isLoggedIn ? (
+              <Navbar.Collapse>
+                <Navbar.Text style={{ fontSize: "0.8rem" }}>
+                  Usuario: {state.nom_usu}
+                </Navbar.Text>
+                <Button className="ms-3" onClick={onLogOut}>
+                  Salir
+                </Button>
+              </Navbar.Collapse>
+            ) : (
+              <React.Fragment></React.Fragment>
+            )}
+          </Navbar>
+        </Container>
       </header>
       {/** Main */}
       <main>{props.children}</main>
@@ -162,6 +165,15 @@ export const PageTemplate = (props) => {
           </p>
         </Container>
       </footer> */}
+      <footer class="container">
+        <p class="float-end">
+          <a href="#">Back to top</a>
+        </p>
+        <p>
+          © 2017–2021 Company, Inc. · <a href="#">Privacy</a> ·{" "}
+          <a href="#">Terms</a>
+        </p>
+      </footer>
     </React.Fragment>
   );
 };
